3ee098ad26bfb8e34c6a96ad4997c00664dc0617
oyuki-prog <oyu8no3jo@gmail.com>
2021-09-13 21:45:37 +0900

    インデントを2から4に変更

--- app/Http/Controllers/CalcsController.php
+++ app/Http/Controllers/CalcsController.php
@@ -6,23 +6,24 @@ use Illuminate\Http\Request;
 
 class CalcsController extends Controller
 {
-  public function result($num1,$operator,$num2)
-  {
-    $result = '';
+    public function result($num1,$operator,$num2)
+    {
+        $result = '';
 
-    if($operator === 'addition') {
-      $result = $num1 + $num2;
-    } elseif($operator === 'subtraction') {
-      $result = $num1 - $num2;
-    } elseif($operator === 'multiplication') {
-      $result = $num1 * $num2;
-    } elseif($operator === 'division') {
-      $result = $num1 / $num2;
-    } else  {
-      $result = '演算子が間違えています';
+        if($operator === 'addition') {
+            $result = $num1 + $num2;
+        } elseif ($operator === 'subtraction') {
+            $result = $num1 - $num2;
+        } elseif ($operator === 'multiplication') {
+            $result = $num1 * $num2;
+        } elseif ($operator === 'division') {
+            $result = $num1 / $num2;
+        } else  {
+            $result = '演算子が間違えています';
+        }
+
+        return view('calculation.calc', [
+            'result' => $result
+        ]);
     }
-    return view('calculation.calc', [
-      'result' => $result
-    ]);
-  }
 }
